5dc1ec7b51ba453bed7277f2658d4d7767e3fc27
username <d@mail.com>
2023-09-29 18:25:31 +0300

    proxy checker method

--- server.py
+++ server.py
@@ -2,22 +2,34 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import urllib
+import json
 
 hostName = "0.0.0.0"
-serverPort = 80
+serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        path = urllib.parse.urlparse(self.path)
-        print(path)
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+	def check_ip(self, query):
+		ip = query.split('=')[1]
+		self.send_response(200)
+		self.send_header("Content-type", "application/json")
+		self.end_headers()
+		self.wfile.write(bytes(json.dumps({'ip': ip}), "utf-8"))
+	def do_GET(self):
+		path = urllib.parse.urlparse(self.path)
+		print(path.query)
+		if path.path == '/check':
+			self.check_ip(path.query)
+			print('________ a am in')
+			return
+        
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+		self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+		self.wfile.write(bytes("<body>", "utf-8"))
+		self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+		self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
